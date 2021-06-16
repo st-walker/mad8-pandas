dea555edda80392052ad89c4f730fb5fb6f9f2ef
stwalker <stuart.walker@desy.de>
2021-06-16 14:17:38 +0200

    added depedency to setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name="pand8",
     version="0.1.0",
     description="load mad8 output with pandas",
-    install_requires=["pandas"],
+    install_requires=["pandas", "fortranformat"],
     license="MIT",
 )
