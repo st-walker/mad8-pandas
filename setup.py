0a5695599282ebf41c6e5f10378586f5ce2620e1
stwalker <stuart.walker@desy.de>
2022-04-12 20:55:11 +0200

    something of a setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,28 @@
 from setuptools import setup
 
 setup(
-    name="pand8",
+    name="mad8-pandas",
     version="0.1.0",
     description="load mad8 output with pandas",
     install_requires=["pandas", "fortranformat", "numpy"],
+    url="https://github.com/st-walker/mad8-pandas",
     license="MIT",
+    keywords="mad8 pandas twiss",
+    author="Stuart Walker",
+    author_email="stuart.walker@desy.de",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Typing :: Typed",
+    ]
 )
